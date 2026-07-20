6308774464c3f4a2bfad193aac5d3bd27e5a3f31
Goofythesecond <vinthedeveloper1@gmail.com>
2026-07-21 02:51:16 +0600

    Task.java is completed and tested now it should only take the amount it can take and send indication if it can't take the amount and it can also store and parse higher level of books

---
+++ src/client/java/com/goofy/goofyaddons/features/bookflipper/helper/Task.java
@@ -0,0 +1,92 @@
+package com.goofy.goofyaddons.features.bookflipper.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task {
+    public enum BookState {
+        SELECTED,
+        IN_BUY_ORDER,
+        OUTBID,
+        ANVIL,
+        COMBINE,
+        SELL,
+        SELL_ORDER,
+        REPLACE_SELL
+    }
+
+    private List<BookPool> bookPool = new ArrayList<>();
+    private Book book;
+    private int amountToOrder;
+    private BookState bookState;
+
+
+    public Task(Book book) {
+        this.book = book;
+        amountToOrder = book.getQtyAmount(book.level());
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public BookState getBookState() {
+        return bookState;
+    }
+
+    public void setBookState(BookState bookState) {
+        this.bookState = bookState;
+    }
+
+    // -1 will indicate failure, 0 will indicate success
+    public int assignBook(Book book, int level, int location, int amountOfBook) {
+        if (book != this.book) return -1;
+        if (book.level() > level) return -1;
+        int amount = parseBookLevel(level);
+        int totalAmount = amount * amountOfBook;
+
+        if (totalAmount > amountToOrder) return -1;
+
+        amountToOrder -= totalAmount;
+
+        for (int i = 0; i < totalAmount; i++) {
+            bookPool.add(new BookPool(level, location));
+        }
+        return 0;
+    }
+
+    public List<BookPool> getBookPool() {
+        return bookPool;
+    }
+
+    public void setBookPool(List<BookPool> list) {
+        this.bookPool = list;
+    }
+
+    public int getAmountToOrder() {
+        return getAmountToOrder();
+    }
+
+
+    private int parseBookLevel(int level) {
+        if (level == book.level()) return 1;
+        return 1 << (level - book.level());
+    }
+
+
+    // book location will be represented in integars, 0 = Inventory, 1 = EnderChest, 2 = EnderChestPage2
+    public class BookPool {
+        int level;
+        int location;
+
+        private BookPool(int level, int location) {
+            this.level = level;
+            this.location = location;
+        }
+
+        public void setLocation(int location) {
+            this.location = location;
+        }
+    }
+
+}
