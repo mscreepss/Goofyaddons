8ff46b23da1c3cb6ae1b3b9e4744b065725f03a8
mscreepss <mscreepss@gmail.com>
2026-08-17 00:11:35 +0300

    Fix condition check for container and clock firing
    added only (), because checking enderchest, jump np not checking in start

--- src/client/java/com/goofy/goofyaddons/features/bookflipper/BazaarFlipper.java
+++ src/client/java/com/goofy/goofyaddons/features/bookflipper/BazaarFlipper.java
@@ -175,7 +175,7 @@ public class BazaarFlipper implements Feature {
                 }
 
                 if (containerCheck("Ender Chest") || containerCheck("Jumbo Backpack") || containerCheck("Greater Backpack")) clock.start(randomizer());
-                if (containerCheck("Ender Chest") || containerCheck("Jumbo Backpack") || containerCheck("Greater Backpack") && clock.shouldFire()) {
+                if ((containerCheck("Ender Chest") || containerCheck("Jumbo Backpack") || containerCheck("Greater Backpack")) && clock.shouldFire()) {
                     List<Book> bookList = new ArrayList<>();
                     bookList.addAll(booksInState(BookState.SELECTED));
 
