c057716c0fb3443c5c1d42cae26e06730cd9be55
mscreepss <mscreepss@gmail.com>
2026-08-17 01:43:10 +0300

    Update GoofyKeybinds.java

--- src/client/java/com/goofy/goofyaddons/keybinds/GoofyKeybinds.java
+++ src/client/java/com/goofy/goofyaddons/keybinds/GoofyKeybinds.java
@@ -14,7 +14,7 @@ public class GoofyKeybinds {
 
     public static KeyMapping startKey;
     public static KeyMapping stopKey;
-    public static KeyMapping reloadConfigKey; // Reload tuşunu ekledik
+    public static KeyMapping reloadConfigKey;
 
     public static void register() {
         startKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
@@ -31,11 +31,17 @@ public class GoofyKeybinds {
                 CATEGORY
         ));
 
-        // R tuşuna atadık, oyuna girdiğinde ayarlardan istediğin tuşla değiştirebilirsin
+        // NOT: GLFW'nin tuş sabitleri (GLFW_KEY_*) fiziksel/konum tabanlıdır ve her zaman
+        // ABD (QWERTY) klavye düzenindeki o karakterin bulunduğu FİZİKSEL tuş konumunu temsil eder;
+        // aktif klavye düzeninden bağımsızdır. Türkçe Q klavyede "/" karakteri Shift+7 ile yazılır,
+        // GLFW_KEY_SLASH ise ABD düzeninde "/" olan fiziksel tuş konumunu (TR'de "." tuşunun olduğu yer)
+        // temsil eder. Minecraft'ın tuş atama menüsünde bu tuş yine de "/" olarak görünecektir.
+        // Eğer gerçekten TR klavyenizde basarken "/" yazdığınız fiziksel tuşu (Shift+7) istiyorsanız
+        // GLFW.GLFW_KEY_7 kullanmanız gerekir; ona göre haber verin, değiştiririm.
         reloadConfigKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
                 "key.goofyaddons.reload_config",
                 InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_R,
+                GLFW.GLFW_KEY_SLASH,
                 CATEGORY
         ));
     }
